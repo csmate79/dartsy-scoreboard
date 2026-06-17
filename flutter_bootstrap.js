c6b1667d78e4a8dcec1fdd632301790d3ca710e2
csmate79 <36413556+csmate79@users.noreply.github.com>
2026-06-17 14:41:17 +0000

    Deploying to main from @ csmate79/dartsy@d72017ff723248fcad36fbfb87d01481ddabab65 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1171830441" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "450891923" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
